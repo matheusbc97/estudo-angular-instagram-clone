a4b3643b21aeb1edc0ce3c052dd04e10b4424617
Matheus Botelho <matheusbc97@users.noreply.github.com>
2020-06-07 00:45:55 -0300

    animações

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,15 +1,101 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, state, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.scss']
+  styleUrls: ['./acesso.component.scss'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(-50px, 0)'
+        }),
+        animate('500ms 0s ease-in-out')
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(50px, 0)'
+        }),
+        animate('1.5s 0s ease-in-out', keyframes([
+            style({
+              offset: 0.15,
+              opacity: 1,
+              transform: 'translateX(0)'
+            }),
+            style({
+              offset: 0.86,
+              opacity: 1,
+              transform: 'translateX(0)'
+            }),
+
+            style({
+              offset: 0.88,
+              opacity: 1,
+              transform: 'translateY(-10px)'
+            }),
+            style({
+              offset: 0.90,
+              opacity: 1,
+              transform: 'translateY(10px)'
+            }),
+            style({
+              offset: 0.92,
+              opacity: 1,
+              transform: 'translateY(-10px)'
+            }),
+            style({
+              offset: 0.94,
+              opacity: 1,
+              transform: 'translateY(10px)'
+            }),
+            style({
+              offset: 0.96,
+              opacity: 1,
+              transform: 'translateY(-10px)'
+            }),
+            style({
+              offset: 0.98,
+              opacity: 1,
+              transform: 'translateY(10px)'
+            }),
+          ]))
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner = 'criado';
+  public estadoPainel = 'criado';
+
+  public cadastro = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public exibirPainel(event: string){
+    this.cadastro = event === 'cadastro';
+  }
+
+  public inicioDaAnimacao(){
+    console.log('inicio');
+  }
+
+  public fimDaAnimacao(){
+    console.log('fim');
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -18,7 +19,8 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
